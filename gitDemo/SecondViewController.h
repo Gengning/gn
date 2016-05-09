cac3c5a05d96f30ed3ea89ad00b1b328e9ed3b52
AnnieBaby <1508559628@qq.com>
2016-05-09 10:26:12 +0800

    添加了SecondVC的界面

---
+++ gitDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  gitDemo
+//
+//  Created by 耿宁－MAC on 16/5/9.
+//  Copyright © 2016年 耿宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
